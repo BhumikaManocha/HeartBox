427ecbe20d8915ac4ef879ceb51f6b96314554ba
BhumikaManocha <bhumika.manocha@gmail.com>
2020-07-17 01:16:47 +0530

    navfoot done 50%

--- src/pages/landing/landing.jsx
+++ src/pages/landing/landing.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import "./landing.css";
+import Navbar from "../../components/navbar"
+import Footer from "../../components/footer"
+
 
 const Landing = () => {
   return (
     <div>
+      <Navbar />
       <h1>Landing page</h1>
+      <Footer />
     </div>
   );
 };
